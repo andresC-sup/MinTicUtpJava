e1f7d9013a92978ca827a18e5856f47afde2f2fb
andresC-sup <camilosama6@gmail.com>
2021-07-01 17:31:37 -0500

    Update App.java
    ejercision semana !

--- firstP/src/App.java
+++ firstP/src/App.java
@@ -5,6 +5,7 @@ public class App {
     
     public static void main(String[] args) throws Exception {
 
+        
         Scanner scan = new Scanner (System.in);  
 
         System.out.println("******************************************");
